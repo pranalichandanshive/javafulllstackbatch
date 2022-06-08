2e46d42dcfdff218fbbb1ca5cd74fe4e7f689bae
pranali28! <you@example.com>
2022-06-08 16:04:10 +0530

    intial commit

---
+++ src/MathematicalOperations.java
@@ -0,0 +1,18 @@
+public class MathematicalOperations {
+    public static void main(String[] args) {
+        int x = 40;
+        int y = 60;
+        int result;
+//        addition
+        result = x + y;
+        System.out.println("Addition is" + result);
+        result = y - x;
+        System.out.println("Substaction is" + result);
+        result = x * y;
+        System.out.println("Multiplication is" + result);
+        result = x / y;
+        System.out.println("Division is" + result);
+
+    }
+
+}
